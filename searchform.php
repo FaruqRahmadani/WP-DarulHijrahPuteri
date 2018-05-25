ca4bbd3b868cff591fea99942ff663a471d78533
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-05-25 22:58:45 +0800

    update css

--- searchform.php
+++ searchform.php
@@ -1,9 +1,5 @@
-<div class="row">
-  <div class="pull-right">
-    <div class="col-md-12">
-      <form id="search" action="<?= home_url( '/' ) ?>" method="get">
-        <input type="search" name="s" id="search" placeholder="search..." value="<?= the_search_query() ?>" style="text-align: center;">
-      </form>
-    </div>
-  </div>
+<div class="text-center">
+  <form id="search" action="<?= home_url( '/' ) ?>" method="get">
+    <input type="search" name="s" id="search" placeholder="search..." value="<?= the_search_query() ?>" style="text-align: center;">
+  </form>
 </div>
